214431a529a9a3df8b3e46326ddb643f944daf62
Pythia <el1vas@yahoo.gr>
2025-12-18 15:12:59 +0100

    Added navigationDiv class and styled it accordingly

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let allEpisodes = []; // Empty array to fill with the retrieved data below
 
 const navigationDiv = document.createElement("div"); // Altered to create a card to include the episodes menu, search box and the text of episode display
+navigationDiv.className = "navigationDiv";
 
 const episodeSelector = document.createElement("select"); // Cretaes the dropdown menu
 episodeSelector.className = "episode-selector"; // Use it for styling after
